c30ea39a45d1bf48aa8cd14f9b986876815b8b3e
Derek Owusu Bekoe <dowusubekoe@gmail.com>
2026-04-28 05:58:35 -0400

    change ssh key type

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,8 @@ output "public_ip" {
   value = azurerm_public_ip.main.ip_address
 }
 
-output "admin_user" {
-  value = var.admin_user
+output "admin_username" {
+  value = var.admin_username
 }
 
 output "db_host" {
